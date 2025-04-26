5285617ddba0d2b8f5eaf14831f43f7aaab99af9
Luis Diaz <machadodiazluis@gmail.com>
2025-04-26 02:00:15 -0400

    chore: added dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends(
+    "next/core-web-vitals",
+    "plugin:react/recommended",
+    "prettier",
+  ),
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,jsx,tsx}"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: __dirname,
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
+  },
+];
+
+export default eslintConfig;
